825460e3a9dc11ec230cacfe225443d0f29bfcdd
Alice Kim <alice828@mail.ubc.ca>
2019-03-24 13:11:33 -0700

    Create roooms service; modified custlanding

--- src/app/components/custlanding/custlanding.component.ts
+++ src/app/components/custlanding/custlanding.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CustUserService } from '../../services/cust-user.service';
+import { RoomsService } from '../../services/rooms.service';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-custlanding',
@@ -9,12 +11,28 @@ import { CustUserService } from '../../services/cust-user.service';
 export class CustlandingComponent implements OnInit {
 
   message = "loading...";
-  constructor(private cust_user: CustUserService) { }
+  constructor(private cust_user: CustUserService, private roomsService: RoomsService, private router: Router) { }
 
   ngOnInit() {
     this.cust_user.getData().subscribe(data => {
       this.message = data.message;
     });
+  }
+
+  getRooms(event): void {
+    event.preventDefault(); 
+    const target = event.target;
+    const date = target.querySelector('#check-in').value;
+    this.roomsService.getAvailableRooms(date).subscribe(data => {
+      const ne: NavigationExtras = {
+        state: {
+          rooms: data
+        }
+      };
+      this.router.navigate(['available'], ne);
+    });
+
+    console.log(date);
   }
 
 }
