33c00c2ed5dbe015feed606148efcaa7fa76904c
d7q0b <aliceykim0828@gmail.com>
2019-03-26 08:24:37 -0700

    Keep data after refresh

--- src/app/components/available/available.component.ts
+++ src/app/components/available/available.component.ts
@@ -12,9 +12,12 @@ export class AvailableComponent implements OnInit {
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     const navigation = this.router.getCurrentNavigation();
     this.rooms = navigation.extras.state ? navigation.extras.state.rooms.data : this.rooms;
+    localStorage.removeItem('Array');
+    localStorage.setItem('Array', JSON.stringify(this.rooms));
   }
 
   ngOnInit() {
+    this.rooms = JSON.parse(localStorage.getItem('Array'));
   }
 
 }
