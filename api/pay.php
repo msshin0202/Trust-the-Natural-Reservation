bf8baf6bf41e1bf7453b7a8cc40ca7aff273a977
Minsoo Shin <msshin0202@gmail.com>
2019-03-27 13:16:54 -0700

    implemented pay features with restrictions

--- api/pay.php
+++ api/pay.php
@@ -24,18 +24,33 @@ if ($isCash) {
 }
 
 try {
-  $sql1 = "INSERT INTO Transaction VALUES({$tid}, '{$paymentType}', {$dateOfTransaction})";
-  $sql2 = "INSERT INTO Pays VALUES({$bid}, {$tid}, {$amount})";
-  if (mysqli_query($con, $sql1)) {
-    $submitResult[RESULT_SUCCESS_KEY] = true;
-    $submitResult[RESULT_MESSAGE_KEY] = "Payment has been processed!";
-    if (!mysqli_query($con, $sql2)) {
+  $insertTransaction = "INSERT INTO Transaction VALUES({$tid}, '{$paymentType}', {$dateOfTransaction})";
+  $insertPays = "INSERT INTO Pays VALUES({$bid}, {$tid}, {$amount})";
+  $checkBill = "SELECT bid FROM Bill b WHERE b.bid = {$bid}";
+  $checkMaxBillAmount = "SELECT b.amount FROM Bill b WHERE {$amount} > b.amount";
+
+  $checkBillResult = mysqli_query($con, $checkBill);
+  $checkBillNumResult = mysqli_num_rows($checkBillResult);
+  $maxBillResult = mysqli_query($con, $checkMaxBillAmount);
+  $maxBillNumOfResult = mysqli_num_rows($maxBillResult);
+  if (mysqli_query($con, $insertTransaction) && mysqli_query($con, $insertPays)) {
+    if ($maxBillNumOfResult !== 0) {
       $submitResult[RESULT_SUCCESS_KEY] = false;
-      $submitResult[RESULT_MESSAGE_KEY] = "Oops! {$bid} is not a valid bill id";
+      $maxBillAmount = mysqli_fetch_assoc($maxBillResult);
+      $submitResult[RESULT_MESSAGE_KEY] = "{$amount} is more than the bill amount! Please enter an amount of {$maxBillAmount["amount"]} or less.";
+    } else {
+      $submitResult[RESULT_SUCCESS_KEY] = true;
+      $submitResult[RESULT_MESSAGE_KEY] = "Payment has been processed!";
     }
   } else {
     $submitResult[RESULT_SUCCESS_KEY] = false;
-    $submitResult[RESULT_MESSAGE_KEY] = "Oops! Something went wrong!";
+    if ($checkBillNumResult === NULL) {
+      $submitResult[RESULT_MESSAGE_KEY] = "Please enter a bill id!";
+    } else if($checkBillNumResult === 0) {
+      $submitResult[RESULT_MESSAGE_KEY] = "{$bid} is not a valid bill id!";
+    } else if ($maxBillNumOfResult === NULL) {
+      $submitResult[RESULT_MESSAGE_KEY] = "Please enter an amount you wish to pay for the bill!";
+    }
   }
 } catch (Exception $e){
   $submitResult[RESULT_SUCCESS_KEY] = false;
