6b02fe7ae2b4867529a8019757bba59fa3610fca
Minsoo Shin <msshin0202@gmail.com>
2019-03-26 20:11:14 -0700

    able to insert into pays and transaction table

--- api/reservation.php
+++ api/reservation.php
@@ -9,21 +9,21 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 
 $params = $_POST['params'];
 $checkIn = $params['date'];
-$fname = $params['fname'];
-$lname = $params['lname'];
-$phonenum = $params['phonenum'];
+$fname = $params['firstName'];
+$lname = $params['lastName'];
+$phonenum = $params['phoneNumber'];
 $rid = mt_rand(1000, 10000);
 
-$sql = "INSERT INTO customers VALUES ('{$phonenum}', '{$fname}', '{$lname}', NULL, NULL)";
+$sql = "INSERT INTO Customer VALUES ('{$phoneNumber}', '{$firstName}', '{$lastName}', NULL, NULL)";
 if ($result = mysqli_query($con, $sql)) {
-  $sql = "INSERT INTO reservations VALUES ({$rid}, '{$phonenum}', {$checkIn}, NULL)";
+  $sql = "INSERT INTO Reservation_Made_By VALUES ({$reservationNumber}, '{$phoneNumber}', {$checkInDate}, NULL)";
   if ($result = mysqli_query($con, $sql)) {
-    $reservation['rid'] = $rid;
+    $reservation['reservationNumber'] = $reservationNumber;
   } else {
-    $reservation['rid'] = 10001;
+    $reservation['reservationNumber'] = 10001;
   }
 } else {
-  $reservation['rid'] = 10001;
+  $reservation['reservationNumber'] = 10001;
 }
 
 echo json_encode(['data'=>$reservation]);
