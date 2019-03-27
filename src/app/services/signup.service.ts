49dfcb14a2252995e627a9433df40b6f9f72e2e7
Min Kyu Choi <minkyuchoi@Minui-MacBookPro.local>
2019-03-27 13:59:59 -0700

    signup page

---
+++ src/app/services/signup.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+
+  baseUrl = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) { }
+
+  submitEmployeeSignUp(firstName, lastName, email, 
+    password, passwordConfirm, gender, role, address) {
+      let signupKind = "employee";
+      return this.http.post<any>(`${this.baseUrl}/signup`, { 
+        signupKind, firstName, lastName, email, 
+        password, passwordConfirm, gender, role, address
+      });
+    }
+
+  submitCustomerSignUp(firstName, lastName, email, phoneNumber,
+    password, passwordConfirm) {
+      let signupKind = "customer";
+      return this.http.post<any>(`${this.baseUrl}/signup`, { 
+        signupKind, firstName, lastName, email, phoneNumber, 
+        password, passwordConfirm
+      });
+    }
+}
