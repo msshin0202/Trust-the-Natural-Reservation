9b0f61d96146372ead72a11773231678dd308719
d7q0b <aliceykim0828@gmail.com>
2019-03-28 00:13:41 -0700

    Fix minor error

--- src/app/services/rooms.service.ts
+++ src/app/services/rooms.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { Room } from './room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomsService {
   baseUrl = 'http://localhost:3000/api';
-  rooms: Room[];
 
   constructor(private http: HttpClient) {}
 
