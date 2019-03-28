ff357dab828f6de089e0ab655b96076fc0c8c0dd
Minsoo Shin <msshin0202@gmail.com>
2019-03-27 22:09:30 -0700

    finished implementing clean feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { SignupComponent } from './components/signup/signup.component';
 import { EloginComponent } from './components/elogin/elogin.component';
 import { EmplandingComponent } from './components/emplanding/emplanding.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ViewDirtyRoomsComponent } from './components/view-dirty-rooms/view-dirty-rooms.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CloginComponent,
     EloginComponent,
     EmplandingComponent,
-    SignupComponent
+    SignupComponent,
+    ViewDirtyRoomsComponent
   ],
   imports: [
     BrowserModule,
@@ -84,10 +86,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
       {
         path: 'employee',
         component: EmplandingComponent
-      }, 
-      { 
+      },
+      {
         path: 'signup',
         component: SignupComponent
+      },
+      {
+        path: 'view-dirty-rooms',
+        component: ViewDirtyRoomsComponent
       }
     ])
   ],
